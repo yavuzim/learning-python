8093cc005b898010eb9757dcd93cdec52d546a9b
yavuzim <kyavuzimer@gmail.com>
2023-03-27 13:31:53 +0300

    1-Python Objeleri ve Veri Yapıları

--- 1-Python Objeleri ve Veri Yapıları/4_veriTip_donusumu.py
+++ 1-Python Objeleri ve Veri Yapıları/4_veriTip_donusumu.py
@@ -1,22 +1,40 @@
-# float'tan int'te
+# float -> int dönüşümü
 
 a = 5.2
 b = int(a)
-print(b) # 5
+print("a : ",a)
+print("b : ",b)
 
-# int'ten float'a
-a = 5
-b = float(a)
-print(b) # 5.0
+# int -> float dönüşümü
 
-# Örnek
+x = 5
+y = float(x)
+print("x : ",x)
+print("y : ",y)
 
-sayi1 = input("1. Sayı : ") # String değer olarak alır.
-sayi2 = input("2. Sayı : ") # String değer olarak alır.
-toplam = sayi1 + sayi2      # sayi1 = 45 sayi2 = 2
-print("Toplam : ",toplam)   # Toplam : 452
+# Örnek 1
 
-x = input("x : ")           # x : 5
-y = input("y : ")           # y : 9
-tplm = int(x) + int(y)      # inputtan alınan değerler int veri tipine dönüştürüldü.
-print("Toplam : ",tplm)     # Toplam : 14
+sayi1 = input("1.Sayı : ")  # sayi1 = 8 | Klavyeden alınan değerler her zaman stringtir.
+sayi2 = input("2.Sayı : ")  # sayi2 = 9 | Klavyeden alınan değerler her zaman stringtir.
+toplam = sayi1 + sayi2      # toplam = 8+9 | Değerleri int'e çevrilmediğinden + operatörü birleştirme yapar.
+print("toplam : ",toplam)   # toplam = 89
+
+# Örnek 2
+
+s1 = input("1.Sayı : ")    # s1 = 8 | Klavyeden alınan değerler her zaman stringtir.
+s2 = input("2.Sayı : ")    # s2 = 9 | Klavyeden alınan değerler her zaman stringtir.
+tplm = int(s1) + int(s2)   # tplm = 8+9 | Değerleri int'e çevrilir + operatörü ile toplama yapar.
+print("toplam : ",tplm)    # tplm = 17
+
+# Örnek 3
+
+deger1 = input("1.Sayı : ")    # deger1 = 8 | Klavyeden alınan değerler her zaman stringtir.
+deger2 = input("2.Sayı : ")    # deger2 = 9 | Klavyeden alınan değerler her zaman stringtir.
+sonuc = int(deger1) + float(deger2)   # sonuc = 8+9 | değer1 int, değer2 floata çevrilir ve toplama yapılır. Sonuç floattır.
+print("toplam : ",sonuc)        # sonuc = 17.0
+
+# Örnek 4
+
+kelime = "yazılım"
+cevir = int(kelime) # Hata verir. Alfabetik değerler sayısal değere çevrilmez.
+print(cevir) 
